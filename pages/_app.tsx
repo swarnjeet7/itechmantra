fc1e5364d0e2a4692254b781f8dcabebbdcca246
Swarnjeet Singh <swarnjeet7@gmail.com>
2022-11-09 00:45:16 +0530

    font issue fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,6 @@
 import "../styles/globals.scss";
-import { Readex_Pro } from "@next/font/google";
 import type { AppProps } from "next/app";
 
-const readexPro = Readex_Pro();
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <style jsx global>{`
-        body {
-          font-family: ${readexPro.style.fontFamily};
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
